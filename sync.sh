8a10a4ccf4a81ec5128cb3078d6280787af09748
Holger Cremer <HolgerCremer@gmail.com>
2014-07-13 19:10:35 +0200

    more excludes

--- sync.sh
+++ sync.sh
@@ -3,7 +3,7 @@
 SERVER=root@v4.kreativitaet-trifft-technik.de:/var/www/
 LOCAL=deploy/
 
-rsync -n -zvcrluPi --delete --exclude '*/album-images/*' --exclude '*/ifs-images/*' --exclude '/cache/**' -e 'ssh -p 2205' $LOCAL $SERVER
+rsync -n -zvcrluPi --delete --exclude-from=sync-ignores -e 'ssh -p 2205' $LOCAL $SERVER
 
 echo ""
 echo "Das war ein dry run! rsync wirklich ausführen?"
@@ -11,5 +11,5 @@ echo "Strg + C zum Abbrechen oder Enter zum fortsetzen"
 read IN
 
 echo "run"
-rsync -zvcrluPi --delete --exclude '*/album-images/*' --exclude '*/ifs-images/*' --exclude '/cache/**' -e 'ssh -p 2205' $LOCAL $SERVER
+rsync -zvcrluPi --delete --exclude-from=sync-ignores -e 'ssh -p 2205' $LOCAL $SERVER
 ssh -p 2205 root@v4.kreativitaet-trifft-technik.de "chmod -R a+r /var/www/"
