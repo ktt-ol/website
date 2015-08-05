ce40d6309c2262d1e83bf4f697faf2883588298f
Holger Cremer <HolgerCremer@gmail.com>
2015-08-05 21:58:37 +0200

    port corrected

--- sync.sh
+++ sync.sh
@@ -3,7 +3,7 @@
 SERVER=root@v4.kreativitaet-trifft-technik.de:/var/www/website/
 LOCAL=deploy/
 
-rsync -n -zvcrluPi --delete --exclude-from=sync-ignores -e 'ssh -p 2205' $LOCAL $SERVER
+rsync -n -zvcrluPi --delete --exclude-from=sync-ignores -e 'ssh -p 2206' $LOCAL $SERVER
 
 echo ""
 echo "Das war ein dry run! rsync wirklich ausführen?"
@@ -12,7 +12,7 @@ read IN
 
 echo "run"
 git log -1 --format="%h - %aN - %ai" > deploy/status.txt
-rsync -zvcrluPi --delete --exclude-from=sync-ignores -e 'ssh -p 2205' $LOCAL $SERVER
+rsync -zvcrluPi --delete --exclude-from=sync-ignores -e 'ssh -p 2206' $LOCAL $SERVER
 rm deploy/status.txt
-ssh -p 2205 root@v4.kreativitaet-trifft-technik.de "chmod -R a+r /var/www/website/ && chmod o+w /var/www/website/sponsors"
+ssh -p 2206 root@v4.kreativitaet-trifft-technik.de "chmod -R a+r /var/www/website/ && chmod o+w /var/www/website/sponsors"
 
